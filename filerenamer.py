743f46cb6cd0e334f22d1be1e05886ae102192cd
alinalin10 <acl84408@uga.edu>
2023-12-29 19:11:40 +0800

    added things

--- filerenamer.py
+++ filerenamer.py
@@ -1,2 +1,16 @@
 from datetime import datetime
 from pathlib import Path
+
+files = Path("/Users/alinalin/Desktop/Files")
+
+for file in files.iterdir():
+    print(f"original filename: {file}")
+    # set key variables for parent path and file extensions
+    directory = file.parent
+    extension = file.suffix
+    # unpack by splitting old name on the '-' character
+    old_name = file.stem
+    region, report_type, old_date = old_name.split('-')
+
+
+
